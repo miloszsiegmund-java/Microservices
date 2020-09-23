4fd94a79d83c97b6464a0646ad6569bf3125f0c1
MILOSZ <milosz.siegmund@gmail.com>
2020-09-23 22:02:00 +0200

    add user token services project

---
+++ user-token-services/src/main/java/com/example/usertokenservices/UserTokenServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.usertokenservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTokenServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTokenServicesApplication.class, args);
+	}
+
+}
