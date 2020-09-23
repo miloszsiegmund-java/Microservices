fa8f22d463ee43a80c53a4458edcf5bc214cf7b3
MILOSZ <milosz.siegmund@gmail.com>
2020-09-23 10:02:26 +0200

    add login api services project

---
+++ login-api-services/src/main/java/com/example/loginapiservices/LoginApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginapiservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApiServicesApplication.class, args);
+	}
+
+}
---
+++ login-api-services/src/test/java/com/example/loginapiservices/LoginApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginapiservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
